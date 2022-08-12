e832fcf5a7a1fe20c9545756c0e6a120ecbcde0b
timtechuz <timur.galiaskarov@timtech.uz>
2022-08-12 14:19:16 +0500

    stage0-module3-taskFinal first attempt

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,29 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+    	int sum = 0, reminder = 0;
+    	//first 
+    	reminder = number%10;
+    	sum += reminder;
+    	number /= 10;
+    	//second 
+    	reminder = number%10;
+    	sum += reminder;
+    	number /= 10;
+    	//third 
+    	reminder = number%10;
+    	sum += reminder;
+    	number /= 10;
+    	//fourth 
+    	reminder = number%10;
+    	sum += reminder;
+    	number /= 10;
+    	System.out.println(sum);
     }
+    /**
+    public static void main(String[] args) {
+    	DigitsSumCalculator dsc = new DigitsSumCalculator();
+    	dsc.calculateSum(5959);
+    }
+    **/    
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,28 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
-    }
+    	int reminder, reverse = 0;
+    	// first digit
+    	reminder = number%10;
+    	reverse = reverse*10+reminder;
+    	number /= 10;
+    	//second digit
+    	reminder = number%10;
+    	reverse = reverse*10+reminder;
+    	number /= 10;
+    	//third digit
+    	reminder = number%10;
+    	reverse = reverse*10+reminder;
+    	number /= 10;
+    	/**
+    	while (number !=0) {
+    		reminder = number%10;
+    		reverse = reverse*10+reminder;
+    		number = number/10;
+    	}
+    	**/
+    	System.out.println(reverse);
+    }    
 }
+
+
